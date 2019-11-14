03d4bbb6972ca932ffca244036ab2434d11ecedd
Jaakko Moisio <jaakko@moisio.fi>
2019-11-14 22:47:02 +0100

    Add doxygen and sphinx

--- cxx-include/enhanced_enum/enhanced_enum.hh
+++ cxx-include/enhanced_enum/enhanced_enum.hh
@@ -1,24 +1,38 @@
+/** \file
+ */
+
 #include <array>
 #include <type_traits>
 
+/** \brief The ennhanced enum library
+ */
 namespace enhanced_enum {
 
+/** \brief Base class for enhanced enum types
+ */
 template<
     typename EnhancedEnum, typename UnderlyingEnum, typename ValueType>
 struct enum_base {
     static_assert( std::is_enum_v<UnderlyingEnum> );
 
-    using enum_type = UnderlyingEnum;
-    using value_type = ValueType;
+    using enum_type = UnderlyingEnum; ///< \brief Underlying enum type
+    using value_type = ValueType;     ///< \brief Enhanced enum value type
 
     enum_base() = default;
+
+    /** \brief Construct enhanced enum from an underlying enumerator
+     */
     constexpr enum_base(UnderlyingEnum name) : name {name} {}
 
+    /** \brief Get the underlying enumerator
+     */
     constexpr enum_type get() const
     {
         return name;
     }
 
+    /** \brief Get the value of the enhanced enumerator
+     */
     constexpr const value_type& value() const
     {
         const auto n = static_cast<std::size_t>(name);
@@ -29,6 +43,8 @@ private:
     UnderlyingEnum name;
 };
 
+/** \brief The enhanced enum type associated with an underlying enum type
+ */
 template<typename Enum>
 using enhanced = decltype(enhance(std::declval<Enum>()));
 
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+Enhanced Enum -- first class enums in C++
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+Docs
+====
+
+.. doxygennamespace:: enhanced_enum
