4fd21ffff14752a9d01d13624fd4534600bdb803
Jaakko Moisio <jaakko@moisio.fi>
2019-11-29 07:05:18 +0100

    Bootstrap Python build smarter
    In order to enable both out-of-source CMake builds and in-source
    builds, do not use configure_file() approach and instead inform
    setup.py script via environment variable that it should do
    out-of-source build.
    
    Out-of-source builds are nice, but in-source builds are the way to go
    when making distributions etc.

---
+++ python/setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+package_dir = os.path.abspath(os.path.dirname(__file__))
+
+def get_description():
+    with open(os.path.join(package_dir, "README")) as f:
+        return f.read()
+
+setup_kwargs = dict(
+    name="EnumECG",
+    version="0.0.1",
+    author="Jaakko Moisio",
+    author_email="jaakko@moisio.fi",
+    description="Generate Enhanced Enum definitions for C++",
+    long_description=get_description(),
+    url="https://github.com/jasujm/enhanced_enum",
+    packages=["enumecg"],
+    package_data={"enumecg": ["templates/*.in"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=["Jinja2>=2.10", "regex", "inflect>=3.0"],
+)
+
+# This is to bootstrap the build in case it is made with Enhanced Enum
+# CMake toolchain
+if os.environ.get("IS_CMAKE_BUILD"):
+    setup_kwargs["package_dir"] = { "": package_dir }
+
+setuptools.setup(**setup_kwargs)
